19ec3fb9bbcb7e6e8db5f92602da9b7903a8496e
ChrisWoo0443 <chefwoodini@gmail.com>
2024-01-13 16:21:09 -0800

    added comments

--- src/shape_main.cpp
+++ src/shape_main.cpp
@@ -5,19 +5,24 @@
 using namespace std;
 
 int main(){
+    //initialize variables
     char shape;
     int height;
     int base;
     int width;
 
+    //ask for shape
     cout << "Triangle or Rectangle? (Enter t/r)" << endl;
     cin >> shape;
 
+    //if triangle
     if(shape == 't'){
         triangle t1;
         cout << "Give me the base" << endl;
         cin >> base;
+        //test for whether the dimension is less than 0 or is not an integer
         while(base < 0 || cin.fail()) {
+            //if not integer clear input
             cin.clear();
             cin.ignore(256, '\n'); 
             cout << "Error, value less than 0 or not an int, try again" << endl;
@@ -26,7 +31,9 @@ int main(){
         t1.set_base(base);
         cout << "give me the height" << endl;
         cin >> height;
+        //test for whether the dimension is less than 0 or is not an integer
         while(height < 0 || cin.fail()) {
+            //if not integer clear input
             cin.clear();
             cin.ignore(256, '\n'); 
             cout << "Error, value less than 0 or not an int, try again" << endl;
@@ -39,7 +46,9 @@ int main(){
         Rectangle r1;
         cout << "Give me the width" << endl;
         cin >> width;
+        //test for whether the dimension is less than 0 or is not an integer
         while(width < 0 || cin.fail()) {
+            //if not integer clear input
             cin.clear();
             cin.ignore(256, '\n'); 
             cout << "Error, value less than 0 or not an int, try again" << endl;
@@ -48,7 +57,9 @@ int main(){
         r1.set_width(width);
         cout << "Give me the height" << endl;
         cin >> height;
+        //test for whether the dimension is less than 0 or is not an integer
         while(height < 0 || cin.fail()) {
+            //if not integer clear input
             cin.clear();
             cin.ignore(256, '\n'); 
             cout << "Error, value less than 0 or not an int, try again" << endl;
@@ -57,6 +68,5 @@ int main(){
         r1.set_height(height);
         cout << "Rectangle area: " << r1.area() << endl;
     }
-
     return 0;
 }
